e38abd33eb484294e52a5c3b876bf7e5bbb86c36
nsjames <DENIED>
2023-06-28 02:23:20 +0100

    fix metamask network adders

--- src/components/ConnectMetaMask/ConnectMetaMask.tsx
+++ src/components/ConnectMetaMask/ConnectMetaMask.tsx
@@ -3,6 +3,43 @@ import React from 'react';
 import styles from './styles.module.css';
 import {Button} from "@site/src/components/Shared/Button/Button";
 
+const addNetwork = (chainId, chainName, rpcUrls, blockExplorerUrls) => {
+    return window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [{
+            chainId,
+            chainName,
+            nativeCurrency: {
+                name: 'EOS',
+                symbol: 'EOS',
+                decimals: 18
+            },
+            rpcUrls,
+            blockExplorerUrls
+        }]
+    }).catch(err => {
+        console.error(err);
+    })
+}
+
+const addMainnet = () => {
+    return addNetwork(
+        '0x4571',
+        'EOS EVM',
+        ['https://api.evm.eosnetwork.com/'],
+        ['https://explorer.evm.eosnetwork.com/']
+    );
+}
+
+const addTestnet = () => {
+    return addNetwork(
+        '0x3cc5',
+        'EOS EVM Testnet',
+        ['https://api.testnet.evm.eosnetwork.com/'],
+        ['https://explorer.testnet.evm.eosnetwork.com/']
+    );
+}
+
 export default function FaucetTokens(){
     return (
         <section className={styles.connectMetamask}>
