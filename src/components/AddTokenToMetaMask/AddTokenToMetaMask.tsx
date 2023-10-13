f182869b631673ac867a5c81bc54980e88334ef5
nsjames <DENIED>
2023-10-13 12:58:13 +0100

    add token to metamask

---
+++ src/components/AddTokenToMetaMask/AddTokenToMetaMask.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import styles from './styles.module.css';
+import {Button} from "@site/src/components/Shared/Button/Button";
+
+interface IProps {
+    address: string;
+    symbol: string;
+    decimals: number;
+}
+
+export default function FaucetTokens({ address, symbol, decimals }: IProps){
+
+    async function addTokenToMetamask() {
+        try {
+            // Check if MetaMask is installed
+            if (!window.ethereum) {
+                alert('MetaMask is not installed');
+                return;
+            }
+
+            await window.ethereum.request({
+                method: 'wallet_watchAsset',
+                params: {
+                    type: 'ERC20',
+                    options: {
+                        address,
+                        symbol,
+                        decimals,
+                        // image: tokenImage,
+                    },
+                },
+            });
+
+        } catch (error) {
+            console.error('Error:', error.message);
+        }
+    }
+
+    return (
+        <Button type={'button'} onClick={() => addTokenToMetamask()}>Add to MetaMask</Button>
+    )
+}
