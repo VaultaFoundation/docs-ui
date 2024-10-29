92e700b26fa53c14e118caa2b36fb1415eba3f8b
nsjames <skyandbeyond@gmail.com>
2024-10-29 11:11:28 -0400

    change logos

--- redocusaurus.specs.js
+++ redocusaurus.specs.js
@@ -24,27 +24,123 @@ module.exports = [
         "route": "/apis/leap/latest/trace_api.api/"
     },
     {
-        "spec": "apis/leap/v3.2.3/chain.swagger.yaml",
-        "route": "/apis/leap/v3.2.3/chain.api/"
+        "spec": "apis/leap/v4.0.6/chain.swagger.yaml",
+        "route": "/apis/leap/v4.0.6/chain.api/"
     },
     {
-        "spec": "apis/leap/v3.2.3/db_size.swagger.yaml",
-        "route": "/apis/leap/v3.2.3/db_size.api/"
+        "spec": "apis/leap/v4.0.6/db_size.swagger.yaml",
+        "route": "/apis/leap/v4.0.6/db_size.api/"
     },
     {
-        "spec": "apis/leap/v3.2.3/net.swagger.yaml",
-        "route": "/apis/leap/v3.2.3/net.api/"
+        "spec": "apis/leap/v4.0.6/net.swagger.yaml",
+        "route": "/apis/leap/v4.0.6/net.api/"
     },
     {
-        "spec": "apis/leap/v3.2.3/producer.swagger.yaml",
-        "route": "/apis/leap/v3.2.3/producer.api/"
+        "spec": "apis/leap/v4.0.6/producer.swagger.yaml",
+        "route": "/apis/leap/v4.0.6/producer.api/"
     },
     {
-        "spec": "apis/leap/v3.2.3/test_control.swagger.yaml",
-        "route": "/apis/leap/v3.2.3/test_control.api/"
+        "spec": "apis/leap/v4.0.6/test_control.swagger.yaml",
+        "route": "/apis/leap/v4.0.6/test_control.api/"
     },
     {
-        "spec": "apis/leap/v3.2.3/trace_api.swagger.yaml",
-        "route": "/apis/leap/v3.2.3/trace_api.api/"
+        "spec": "apis/leap/v4.0.6/trace_api.swagger.yaml",
+        "route": "/apis/leap/v4.0.6/trace_api.api/"
+    },
+    {
+        "spec": "apis/leap/v4.0.6/chain.swagger.yaml",
+        "route": "/apis/leap/v4.0.6/chain.api/"
+    },
+    {
+        "spec": "apis/leap/v4.0.6/db_size.swagger.yaml",
+        "route": "/apis/leap/v4.0.6/db_size.api/"
+    },
+    {
+        "spec": "apis/leap/v4.0.6/net.swagger.yaml",
+        "route": "/apis/leap/v4.0.6/net.api/"
+    },
+    {
+        "spec": "apis/leap/v4.0.6/producer.swagger.yaml",
+        "route": "/apis/leap/v4.0.6/producer.api/"
+    },
+    {
+        "spec": "apis/leap/v4.0.6/test_control.swagger.yaml",
+        "route": "/apis/leap/v4.0.6/test_control.api/"
+    },
+    {
+        "spec": "apis/leap/v4.0.6/trace_api.swagger.yaml",
+        "route": "/apis/leap/v4.0.6/trace_api.api/"
+    },
+    {
+        "spec": "apis/leap/v3.2.6/chain.swagger.yaml",
+        "route": "/apis/leap/v3.2.6/chain.api/"
+    },
+    {
+        "spec": "apis/leap/v3.2.6/db_size.swagger.yaml",
+        "route": "/apis/leap/v3.2.6/db_size.api/"
+    },
+    {
+        "spec": "apis/leap/v3.2.6/net.swagger.yaml",
+        "route": "/apis/leap/v3.2.6/net.api/"
+    },
+    {
+        "spec": "apis/leap/v3.2.6/producer.swagger.yaml",
+        "route": "/apis/leap/v3.2.6/producer.api/"
+    },
+    {
+        "spec": "apis/leap/v3.2.6/test_control.swagger.yaml",
+        "route": "/apis/leap/v3.2.6/test_control.api/"
+    },
+    {
+        "spec": "apis/leap/v3.2.6/trace_api.swagger.yaml",
+        "route": "/apis/leap/v3.2.6/trace_api.api/"
+    },
+    {
+        "spec": "apis/leap/v3.2.6/chain.swagger.yaml",
+        "route": "/apis/leap/v3.2.6/chain.api/"
+    },
+    {
+        "spec": "apis/leap/v3.2.6/db_size.swagger.yaml",
+        "route": "/apis/leap/v3.2.6/db_size.api/"
+    },
+    {
+        "spec": "apis/leap/v3.2.6/net.swagger.yaml",
+        "route": "/apis/leap/v3.2.6/net.api/"
+    },
+    {
+        "spec": "apis/leap/v3.2.6/producer.swagger.yaml",
+        "route": "/apis/leap/v3.2.6/producer.api/"
+    },
+    {
+        "spec": "apis/leap/v3.2.6/test_control.swagger.yaml",
+        "route": "/apis/leap/v3.2.6/test_control.api/"
+    },
+    {
+        "spec": "apis/leap/v3.2.6/trace_api.swagger.yaml",
+        "route": "/apis/leap/v3.2.6/trace_api.api/"
+    },
+    {
+        "spec": "apis/spring/latest/chain.swagger.yaml",
+        "route": "/apis/spring/latest/chain.api/"
+    },
+    {
+        "spec": "apis/spring/latest/db_size.swagger.yaml",
+        "route": "/apis/spring/latest/db_size.api/"
+    },
+    {
+        "spec": "apis/spring/latest/net.swagger.yaml",
+        "route": "/apis/spring/latest/net.api/"
+    },
+    {
+        "spec": "apis/spring/latest/producer.swagger.yaml",
+        "route": "/apis/spring/latest/producer.api/"
+    },
+    {
+        "spec": "apis/spring/latest/test_control.swagger.yaml",
+        "route": "/apis/spring/latest/test_control.api/"
+    },
+    {
+        "spec": "apis/spring/latest/trace_api.swagger.yaml",
+        "route": "/apis/spring/latest/trace_api.api/"
     }
 ];
